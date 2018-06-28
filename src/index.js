798862a73abf99732c073572630041971fc0d86f
tianliang <598318876@qq.com>
2018-06-28 10:06:38 +0800

    test-api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser'
+import auth from './routes/auth';
 
-const app = new express();
+const app = express();
 
-app.post("/api/auth", (req,res) => {
-  res.status(400).json({errors:{global: "Invalid credentials"}});
-});
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/test");
+
+app.use("/api/auth", auth);
+
+// app.post("/api/auth", (req, res) => {
+//   res.status(400).json({errors: {global: "Invalid"}})
+// });
 
 app.get("/*", (req,res) => {
   res.sendFile(path.join(__dirname, "index.html"));
